6418b5855c4a64bf10d0dcd749910d8939049598
pradeep dhane <pradeepdhane3@gmail.com>
2024-07-29 16:48:57 +0530

    revert something

--- GitRepo/index.js
+++ GitRepo/index.js
@@ -1,10 +1,13 @@
 const age=98;
-const name1=dhane;
-console.log(`Hello, my name is ${name1}. I am ${age} years old.`);
+const name=dhane;
+console.log(`Hello, my name is ${name}. I am ${age} years old.`);
 const aajoba="hardwarking aajoba";
 console.log(`I love to work in ${aajoba}.`);
+<<<<<<< HEAD
 
 
 function newfunctions() {
   console.log("hey i am a new function")
 }
+=======
+>>>>>>> parent of 6b64adb (Added isAbove18 func)
