6b64adbb94020d95df9ce4353ec72da337bd48be
pradeep dhane <pradeepdhane3@gmail.com>
2024-07-29 15:43:47 +0530

    Added isAbove18 func

--- GitRepo/index.js
+++ GitRepo/index.js
@@ -1,5 +1,13 @@
 const age=98;
-const name=dhane;
-console.log(`Hello, my name is ${name}. I am ${age} years old.`);
+const name1=dhane;
+console.log(`Hello, my name is ${name1}. I am ${age} years old.`);
 const aajoba="hardwarking aajoba";
 console.log(`I love to work in ${aajoba}.`);
+
+
+function isAbove18(){
+  if(age>=18){
+    return true;
+  }
+  return false;
+}
