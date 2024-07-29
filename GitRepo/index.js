2f27667fab37a631bfa2760b6646a677ec3d9373
pradeep dhane <pradeepdhane3@gmail.com>
2024-07-29 15:36:24 +0530

    initial code added

---
+++ GitRepo/index.js
@@ -0,0 +1,5 @@
+const age=98;
+const name=dhane;
+console.log(`Hello, my name is ${name}. I am ${age} years old.`);
+const aajoba="hardwarking aajoba";
+console.log(`I love to work in ${aajoba}.`);
---
+++ GitRepo/script.js
@@ -0,0 +1,6 @@
+let a=9;
+let b=9;
+
+function sum(a,b){
+  return a+b;
+}
