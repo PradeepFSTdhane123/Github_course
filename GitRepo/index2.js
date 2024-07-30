930539f10442b3b7e2ddcc42de248f6204ee66c0
pradeep dhane <pradeepdhane3@gmail.com>
2024-07-30 15:57:05 +0530

    new console add

--- GitRepo/index2.js
+++ GitRepo/index2.js
@@ -2,5 +2,7 @@ function call(){
   console.log("my name is xyz")
 }
 function newFeatureAdd(){
-  console.log("i am adding new feature")
+  console.log("i am adding new feature");
+  console.log("my name is pqr");
+
 }
